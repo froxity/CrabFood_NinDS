07592f9a68a7b74b219a8164fcb79af50cd0c778
Jason Cheng <jasoncwj@hotmail.com>
2019-05-01 01:38:51 +0800

    Changed index -> branchIndex

--- src/crabfood/event/OrderTakenEvent.java
+++ src/crabfood/event/OrderTakenEvent.java
@@ -8,11 +8,11 @@ public class OrderTakenEvent extends Event {
     private int posX;
     private int posY;
 
-    public OrderTakenEvent(Restaurant restaurant, int index, int eventTime) {
+    public OrderTakenEvent(Restaurant restaurant, int branchIndex, int eventTime) {
         super.setEventTime(eventTime);
         this.restaurant = restaurant;
-        posX = (int)restaurant.getBranch(index).getX();
-        posY = (int)restaurant.getBranch(index).getY();
+        posX = (int)restaurant.getBranch(branchIndex).getX();
+        posY = (int)restaurant.getBranch(branchIndex).getY();
     }
 
     @Override
