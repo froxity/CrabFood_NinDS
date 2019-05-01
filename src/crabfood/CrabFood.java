5eb53f62486912ac797deaf11000b12aeed65e62
Jason Cheng <jasoncwj@hotmail.com>
2019-05-01 12:19:54 +0800

    Cleanup code

--- src/crabfood/CrabFood.java
+++ src/crabfood/CrabFood.java
@@ -9,22 +9,22 @@ public class CrabFood {
         LinkedList<Restaurant> restaurantList = new LinkedList<>();
         LinkedList<Customer> customerList = new LinkedList<>();
         Generator maker = new Generator();
-        
+
         //Read file and create all restaurants
         maker.generateRestaurantList(restaurantList);
-        
+
         //Display info of all restaurants
-        for(int i = 0; i < restaurantList.size(); i++){
+        for (int i = 0; i < restaurantList.size(); i++) {
             System.out.println(restaurantList.get(i).toString());
         }
-        
+
         //Map generation
         maker.generateMap(restaurantList);
         maker.printMap();
-        
+
         //Customer side
         maker.generateCustomerList(customerList);
-        
+
         //Start the day
         maker.startDay(customerList, restaurantList);
     }
