b057de1b0f96d7185d2341922cf844a52153450e
Jason Cheng <jasoncwj@hotmail.com>
2019-05-01 15:53:52 +0800

    Added Javadoc

--- src/crabfood/Restaurant.java
+++ src/crabfood/Restaurant.java
@@ -2,19 +2,20 @@ package crabfood;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
 /**
- * Restaurant is the class that stores the name of the restaurant, its branches 
- * and its menu. The branches are stored in an ArrayList consisting of objects of 
- * internal class known as {@code Branch}. Each branch stores the x and y 
+ * Restaurant is the class that stores the name of the restaurant, its branches
+ * and its menu. The branches are stored in an ArrayList consisting of objects
+ * of internal class known as {@code Branch}. Each branch stores the x and y
  * coordinates and the availability time if an order was placed for it.<p>
- * 
- * The {@code foodMenu} is stored as a HashMap with the name of the food as the key and
- * the time taken to prepare the food as the value.
- * 
+ *
+ * The {@code foodMenu} is stored as a HashMap with the name of the food as the
+ * key and the time taken to prepare the food as the value.
+ *
  * @param name the name of the restaurant
  * @param branchList the list of branches
  * @param foodMenu the available menu of said restaurant
- * 
+ *
  * @author User
  */
 public class Restaurant {
@@ -60,8 +61,9 @@ public class Restaurant {
     private HashMap<String, Integer> foodMenu;
 
     /**
-     * Creates a restaurant 
-     * @param name 
+     * Creates a restaurant with a specified name.
+     *
+     * @param name name of restaurant
      */
     public Restaurant(String name) {
         this.name = name;
@@ -69,34 +71,79 @@ public class Restaurant {
         foodMenu = new HashMap<>();
     }
 
+    /**
+     * Returns the name of the restaurant.
+     *
+     * @return the name of the restaurant
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets a new restaurant name
+     *
+     * @param name the name of the restaurant
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Create a new branch for the restaurant with specified coordinates.
+     *
+     * @param x x-coordinate of the new branch
+     * @param y y-coordinate of the new branch
+     */
     public void addBranch(int x, int y) {
         branchList.add(new Branch(x, y));
     }
 
+    /**
+     * Returns the total number of branches.
+     *
+     * @return the total number of branches
+     */
     public int getBranchTotal() {
         return branchList.size();
     }
 
+    /**
+     * Returns the specified restaurant branch
+     *
+     * @param branchNo index of restaurant branch
+     * @return the specified branch of the restaurant
+     */
     public Branch getBranch(int branchNo) {
         return branchList.get(branchNo);
     }
 
+    /**
+     * Returns the whole list of branches.
+     *
+     * @return the list of branches
+     */
     public ArrayList<Branch> getBranchList() {
         return branchList;
     }
 
+    /**
+     * Add a food item to the menu list with the time taken to prepare it.
+     *
+     * @param foodItem the name of the food
+     * @param timeTaken the time taken to prepared the food
+     */
     public void addMenuItem(String foodItem, int timeTaken) {
         foodMenu.put(foodItem, timeTaken);
     }
 
+    /**
+     * Returns the preparation time for a certain food.
+     *
+     * @param foodItem the name of the food item.
+     * @return the time taken to prepare the food. Returns 0 if it does not
+     * exist.
+     */
     public int getPrepTime(String foodItem) {
         if (foodMenu.containsKey(foodItem)) {
             return foodMenu.get(foodItem);
