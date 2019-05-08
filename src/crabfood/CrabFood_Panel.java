9790cbd614c07003d75d394a39b3a024652d41c4
froxity <45290620+froxity@users.noreply.github.com>
2019-05-09 04:08:51 +0800

    change path icon and read path file images

--- src/crabfood/CrabFood_Panel.java
+++ src/crabfood/CrabFood_Panel.java
@@ -34,7 +34,7 @@ public class CrabFood_Panel extends JPanel {
         Graphics2D g2d = (Graphics2D) g; //for better graphics
 
         //getting the source image
-        ImageIcon roadIcon = new ImageIcon("images\\road.png"); //grey color
+        ImageIcon roadIcon = new ImageIcon("images\\road2.png"); //grey color
         ImageIcon ccIcon = new ImageIcon("images\\crusty_crab.png", "Crusty Crab"); //choc color
         ImageIcon pbIcon = new ImageIcon("images\\phum_bucket.png", "Phum Bucket"); //green color
         ImageIcon bkIcon = new ImageIcon("images\\burger_krusty.png", "Burger Krusty"); //blue color
